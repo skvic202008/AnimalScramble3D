785ebe0c9c7b274f3869f815432cab984a3ea576
skvic202008 <skvic202008@gmail.com>
2015-03-27 23:47:12 -0500

    android chages

---
+++ Assets/ShowAds.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class ShowAds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+		Advertisement.Initialize ("28617");
+		ShowUnityAds ();
+	}
+
+	public void ShowUnityAds()
+	{
+		if(Advertisement.isReady()){ Advertisement.Show(); } 
+		}
+	
+
+}
